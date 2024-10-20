0eb4e70d350d6aa97e0ab895cc8d227ee6cece34
SuvajitMajhi <suvajit.majhi19.work@gmail.com>
2024-10-20 10:23:12 +0530

    done

--- target/classes/com/revature/script.js
+++ target/classes/com/revature/script.js
@@ -3,8 +3,9 @@
 
 function numberMethod() {   
     // TODO: Declare and initialize a number variable named 'myNumber' with a value of 3.14
+    let myNumber = 3.14;
     let outputElement = document.getElementById("output");
-    outputElement.textContent = `Number: ${"Replace me with the variable you've just created"}`;
+    outputElement.textContent = `Number: ${myNumber}`;
 }
 
 // Undefined Datatype
@@ -12,8 +13,9 @@ function numberMethod() {
 
 function undefinedMethod() {
     // TODO: Declare an undefined variable named 'myUndefined' and display its type
+    let myUndefined;
     let outputElement = document.getElementById("output");
-    outputElement.textContent = `Undefined: ${typeof "Replace me with the variable you've just created"}`;
+    outputElement.textContent = `Undefined: ${typeof myUndefined}`;
 }
 
 // Null Datatype
@@ -21,8 +23,9 @@ function undefinedMethod() {
 
 function nullMethod() {
     // TODO: Declare a null variable named 'myNull' and display its value
+    let myNull = null;
     let outputElement = document.getElementById("output");
-    outputElement.textContent = `Null: ${"Replace me with the variable you've just created"}`;
+    outputElement.textContent = `Null: ${myNull}`;
 }
 
 // Object Datatype
@@ -30,8 +33,9 @@ function nullMethod() {
 
 function objectMethod(name, age) {
     // TODO: Declare and initialize an object variable named 'myObject' with properties name and age, assign it with the parameters accordingly.
+    let myObject ={name : name , age: age}
     let outputElement = document.getElementById("output");
-    outputElement.textContent = `Object: ${JSON.stringify("Replace me with the variable you've just created")}`;
+    outputElement.textContent = `Object: ${JSON.stringify(myObject)}`;
 }
 
 
@@ -40,6 +44,7 @@ function objectMethod(name, age) {
 
 function arrayMethod() {
     // TODO: Declare and initialize an array variable named 'myArray' with values from 1 to 5
+    let myArray=[1,2,3,4,5];
     let outputElement = document.getElementById("output");
-    outputElement.textContent = `Array: ${JSON.stringify("Replace me with the variable you've just created")}`;
+    outputElement.textContent = `Array: ${JSON.stringify(myArray)}`;
 }
